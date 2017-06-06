f194f6de51275a976b7b67eba2d3f89098b38902
Nicolas Thiébaut <nkthiebaut@gmail.com>
2017-06-07 01:59:22 +0200

    update README

--- out.py
+++ out.py
@@ -1,17 +1,20 @@
 # coding: utf-8
-# In[20]:
+# In[4]:
 import re
 from nltk.tokenize import wordpunct_tokenize
 import numpy as np
+
+from time import time
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.linear_model import LogisticRegression, Lasso, RidgeClassifier, ElasticNet
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix, make_scorer
+from sklearn.pipeline import Pipeline, make_pipeline, FeatureUnion
 raw_corpus = open('corpus.txt', 'r', encoding='utf-8-sig').read()
 # ## Pre-processing
-# In[4]:
+# In[5]:
 title_match_regex = '\n{3,}\s+THE SECRET CACHE\n{3,}.*' # used to remove headers, toc, etc.
 corpus = re.search(title_match_regex, raw_corpus, flags=re.M+re.S).group()
 corpus = corpus.replace('\n', ' ') 
@@ -19,10 +22,10 @@ corpus = re.sub(r' {2,}', ' ', corpus) # replace multiple blanks by one
 corpus = corpus.replace('----', '') # remove consecutive hyphens that we'll as a tag for the be verb
 
 
-# In[5]:
+# In[6]:
 
 # ## Training set creation
-# In[18]:
+# In[155]:
 be_forms = ['am','are','were','was','is','been','being','be']
 substitute = '----'
 tokens = wordpunct_tokenize(corpus)
@@ -36,42 +39,64 @@ targets = find_targets(tokens)
 tokens = remove_targets(tokens)
 def create_windows(tokens, window_size=5):
     """ Create windows surrouding be forms. """
+    for i in range(window_size):
+        tokens.insert(0, 'BEGINNING')
+    tokens.extend(['END']*window_size)
     contexts = []
     for i, word in enumerate(tokens):
         if word == substitute:
             window = tokens[i-window_size:i] + tokens[i+1:i+window_size+1]
             window = ' '.join(window)
             contexts.append(window)    
-    return contexts
-contexts = create_windows(tokens)
+    return np.array(contexts).reshape(-1, 1)
+contexts = create_windows(tokens, window_size=3)
 
 
 counts = np.unique(targets, return_counts=True)
-#for form, count in zip(counts[0], counts[1]):
 
-# In[15]:
+    
 # Replace target names with integer label
-le = LabelEncoder()
-y = le.fit_transform(targets)
+target_encoder = LabelEncoder()
+y = target_encoder.fit_transform(targets)
 # ## Modelling
-# In[19]:
+# In[156]:
 contexts_train, contexts_test, y_train, y_test = train_test_split(contexts, y, test_size=0.3)
-# In[24]:
-# Vectorize context features
-#vectorizer = TfidfVectorizer(max_df=0.5, max_features=10000, min_df=2, stop_words='english')
-vectorizer = CountVectorizer()
-classifier = LogisticRegression()
-pipe = make_pipeline(vectorizer, classifier)
-pipe.fit(contexts_train, y_train)
-y_pred = pipe.predict(contexts_test)
-
+def avg_accuracy(y_test, y_pred):
+    """ Average classes' prediction accuracy. It is a custom score that is not 
+    remniscient of the training set class imbalance. """
+    conf_mat = confusion_matrix(y_test, y_pred)
+    return np.mean(conf_mat.diagonal()/conf_mat.sum(axis=1))
+# ### Fit one model
+# In[157]:
+from sklearn.preprocessing import FunctionTransformer
+def texts_left(texts):
+    cut_size = len(texts[0][0].split())//2
+    return [' '.join(t[0].split()[:cut_size]) for t in texts]
+def texts_right(texts):
+    cut_size = len(texts[0][0].split())//2
+    return [' '.join(t[0].split()[cut_size:]) for t in texts]
+# In[163]:
+left_vectorizer = TfidfVectorizer(max_df=0.5, max_features=3000, min_df=2, stop_words=None)
+right_vectorizer = TfidfVectorizer(max_df=0.5, max_features=3000, min_df=2, stop_words=None)
+classifier = LogisticRegression(C=0.5, class_weight='balanced')
+pipeline = Pipeline([
+    ('union', FeatureUnion(transformer_list=[
+                 ('left_bow', Pipeline([('cut_left', FunctionTransformer(texts_left)),
+                                        ('vectorizer_left', left_vectorizer)
+                                       ]),),
+                 ('right_bow', Pipeline([('cut_right', FunctionTransformer(texts_right)),
+                                         ('vectorizer_right', right_vectorizer)]))])),
+    ('clf', classifier)])
+pipeline.fit(contexts_train, y_train)
+y_pred_train = pipeline.predict(contexts_train)
+y_pred = pipeline.predict(contexts_test)
 
 # ## Prediction on sample
-# In[27]:
-sample = open('sample_input.txt', 'r', encoding='utf-8-sig').read().splitlines()
-N = int(sample[0])
-sample = sample[1]
-sample = wordpunct_tokenize(sample)
-contexts_sample = create_windows(sample)
-sample_pred = le.inverse_transform(pipe.predict(contexts_sample))
+# Read from STDIN (for submission on HackerRank)
+import fileinput
+f = fileinput.input()
+N = int(f.readline())
+sample = f.readline()
+contexts_sample = create_windows(wordpunct_tokenize(sample))
+sample_pred = target_encoder.inverse_transform(pipeline.predict(contexts_sample))
 print('\n'.join(sample_pred))
